efb9922dc1c4e8931c80125e8972cd55d407b90d
Danjela Lura <danielaluraa@gmail.com>
2020-06-16 21:03:03 +0200

    Swap arguments around in some procedures

--- guix-data-service/model/package-metadata.scm
+++ guix-data-service/model/package-metadata.scm
@@ -31,7 +31,7 @@
   #:use-module (guix-data-service model utils)
   #:export (select-package-metadata-by-revision-name-and-version
             inferior-packages->package-metadata-ids
-            
+
             package-description-and-synopsis-locale-options-guix-revision))
 (define locales
   '("cs_CZ.utf8"
@@ -201,7 +201,8 @@ WHERE packages.id IN (
                  " RETURNING id"
                  ";"))
 
-(define (inferior-packages->translated-package-descriptions-and-synopsis inferior-package-id inferior)
+(define (inferior-packages->translated-package-descriptions-and-synopsis inferior
+                                                                         inferior-package-id)
 
   (define (translate inferior-package-id)
     `(let* ((package (hashv-ref %package-table ,inferior-package-id))
@@ -357,26 +358,25 @@ WHERE packages.id IN (
         (insert-package-description-set conn package-description-ids))))))
 
 (define (inferior-packages->package-metadata-ids conn
+                                                 inferior
                                                  packages
-                                                 license-set-ids
-                                                 inferior)
+                                                 license-set-ids)
   (define package-metadata
     (map (lambda (package license-set-id)
-           (let ((translated-package-descriptions-and-synopsis
-                  (inferior-packages->translated-package-descriptions-and-synopsis
-                   inferior (inferior-package-id package))))
-               (list (non-empty-string-or-false
-                      (inferior-package-home-page package))
-                     (location->location-id
-                      conn
-                      (inferior-package-location package))
-                     license-set-id
-                     (package-description-data->package-description-set-id
-                      conn
-                      (car translated-package-descriptions-and-synopsis))
-                     (package-synopsis-data->package-synopsis-set-id
-                      conn
-                      (cdr translated-package-descriptions-and-synopsis)))))
+           (list (non-empty-string-or-false
+                  (inferior-package-home-page package))
+                 (location->location-id
+                  conn
+                  (inferior-package-location package))
+                 license-set-id
+                 (package-description-data->package-description-set-id
+                  conn
+                  (car (inferior-packages->translated-package-descriptions-and-synopsis
+                        inferior (inferior-package-id package))))
+                 (package-synopsis-data->package-synopsis-set-id
+                  conn
+                  (cdr (inferior-packages->translated-package-descriptions-and-synopsis
+                        inferior (inferior-package-id package))))))
          packages
          license-set-ids))
 
